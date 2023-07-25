40f15aea393054c2d58643534024b5b7a429b54b
Sorawit Suriyakarn <thepsint@gmail.com>
2023-07-25 13:18:08 +0700

    Add ability to train

---
+++ examples/ex_predict_animal.py
@@ -0,0 +1,25 @@
+import os
+from langchain.chat_models import ChatOpenAI
+from llfn import LLFn
+
+
+function_prompt = LLFn()
+
+
+llm = ChatOpenAI(
+    temperature=0.7,
+    openai_api_key=os.getenv("OPENAI_API_KEY"),
+    model="gpt-3.5-turbo",
+)  # type: ignore
+
+
+@function_prompt
+def predict_animal(text: str) -> str:
+    return f"What animal fits the following description the best: {text}"
+
+
+predict_animal.train("It has four legs and barks")("obviously a dog")
+predict_animal.train("It has four legs and meows")("obviously a cat")
+
+function_prompt.bind(llm)
+print(predict_animal("It has two legs and flies"))  # obviously a bird
--- llfn/llfn.py
+++ llfn/llfn.py
@@ -1,8 +1,9 @@
 from functools import update_wrapper
 from langchain.llms.base import BaseLLM
 from langchain.chat_models.base import BaseChatModel
-from langchain.schema import HumanMessage, SystemMessage
+from langchain.schema import BaseMessage, HumanMessage, SystemMessage
 from pydantic import BaseModel, Field
+from typing import List
 
 
 class LLFnFunc:
@@ -10,6 +11,7 @@ class LLFnFunc:
         self.app = app
         self.func = func
         self.llm = None
+        self.examples = []
 
         class Result(BaseModel):
             result: func.__annotations__["return"] = Field(
@@ -21,6 +23,14 @@ class LLFnFunc:
 
     def bind(self, llm):
         self.llm = llm
+
+    def train(self, *args, **kwargs):
+        def wrapper(inner_result):
+            prompt = self.func(*args, **kwargs)
+            expected_result = self.result_type(result=inner_result)
+            self.examples.append((prompt, expected_result.json()))
+
+        return wrapper
 
     def __call__(self, *args, **kwargs):
         if self.llm is not None:
@@ -35,22 +45,26 @@ class LLFnFunc:
         system_prompt = f"""
 - You MUST process the user's command and produce exactly one result without any other contexts or explanations
 - Your output must be a JSON dump of a Pydantic object of schema: {self.result_type.schema()}
-- Output format is EXTREMELY important. Make sure it's a valid JSON dump of the Pydantic object
+- Output format is EXTREMELY important. Make sure it's a valid JSON dump of the Pydantic object. Do not include things like
 """
         if isinstance(llm, BaseChatModel):
-            output = llm.predict_messages(
-                [
-                    SystemMessage(content=system_prompt),
-                    HumanMessage(content=user_prompt),
-                ]
-            ).content
+            messages: List[BaseMessage] = [SystemMessage(content=system_prompt)]
+            for prompt, expected_result in self.examples:
+                messages.append(
+                    HumanMessage(
+                        content=f"""
+Example command: {prompt}
+Expected response: {expected_result}"""
+                    )
+                )
+            messages.append(HumanMessage(content=user_prompt))
+            output = llm.predict_messages(messages).content
         elif isinstance(llm, BaseLLM):
-            output = llm(
-                f"""{system_prompt}
-
-User command: {user_prompt}""
-"""
-            )
+            message = f"{system_prompt}\n\n"
+            for prompt, expected_result in self.examples:
+                message += f"Example command: {prompt}\n\n{expected_result}\n\n"
+            message += f"User command: {user_prompt}\n\n"
+            output = llm(message)
         else:
             raise ValueError(f"LLM must be either a ChatModel or a BaseLLM")
         return self.result_type.parse_raw(output).result
