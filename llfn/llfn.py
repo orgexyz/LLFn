bae73ba665870947c567a2d5d36efcd6dc0bee3c
Sorawit Suriyakarn <thepsint@gmail.com>
2023-07-26 13:51:10 +0700

    Refactor runs on different LLMs to _run_completion function

--- llfn/llfn.py
+++ llfn/llfn.py
@@ -1,18 +1,52 @@
 import inspect
-from typing import Callable, List, Optional, Type, Tuple
+from typing import Callable, List, Optional, Type, Tuple, Union
 from functools import update_wrapper
 
 from langchain.llms.base import BaseLLM
 from langchain.chat_models.base import BaseChatModel
-from langchain.schema import BaseMessage, HumanMessage, SystemMessage
+from langchain.schema import BaseMessage, HumanMessage, SystemMessage, AIMessage
 from pydantic import BaseModel, Field
+
+
+def _run_completion(
+    llm: Union[BaseChatModel, BaseLLM], messages: List[BaseMessage]
+) -> str:
+    """
+    Run a completion on the given LLM with the given messages.
+
+    Args:
+        llm: The LLM to run the completion on. Must be either a ChatModel or a BaseLLM.
+        messages: The messages to run the completion on.
+
+    Returns:
+        The completion result as a string.
+    """
+
+    if isinstance(llm, BaseChatModel):
+        return llm.predict_messages(messages).content
+    elif isinstance(llm, BaseLLM):
+        texts = []
+        for message in messages:
+            if message.type == "system":
+                texts.append(f"System: {message.content}")
+            elif message.type == "human":
+                texts.append(f"User: {message.content}")
+            elif message.type == "ai":
+                texts.append(f"Assistant: {message.content}")
+            else:
+                raise ValueError(f"Unknown message type: {message.type}")
+        texts.append("Assistant: ")
+        return llm("\n\n".join(texts))
+    else:
+        raise ValueError(f"LLM must be either a ChatModel or a BaseLLM; got {llm}")
 
 
 class LLFnFunc:
     """
-    This class is a wrapper around a user-provided function that is used to generate a prompt.
-    LLFnFunc can be invoked as a function, which in turn invokes the user-provided function and
-    passes the result to the LLM. The result will be parsed and return back to the caller.
+    This class is a wrapper around a user-provided function that is used to generate
+    a prompt. LLFnFunc can be invoked as a function, which in turn invokes the
+    user-provided function and passes the result to the LLM. The result will be parsed
+    and return back to the caller.
     """
 
     app: "LLFn"
@@ -69,28 +103,12 @@ class LLFnFunc:
 - Your output must be a JSON dump of a Pydantic object of schema: {self.result_type.schema()}
 - Output format is EXTREMELY important. Make sure it's a valid JSON dump of the Pydantic object.
 """
-        if isinstance(llm, BaseChatModel):
-            messages: List[BaseMessage] = [SystemMessage(content=system_prompt)]
-            for prompt, expected_result in self.examples:
-                messages.append(
-                    HumanMessage(
-                        content=f"""
-Example command: {prompt}\n\n
-{expected_result}"""
-                    )
-                )
-            messages.append(HumanMessage(content=user_prompt))
-            output = llm.predict_messages(messages).content
-        elif isinstance(llm, BaseLLM):
-            message = f"{system_prompt}\n\n"
-            for prompt, expected_result in self.examples:
-                message += f"""
-Example command: {prompt}\n\n
-{expected_result}\n\n"""
-            message += f"{user_prompt}"
-            output = llm(message)
-        else:
-            raise ValueError(f"LLM must be either a ChatModel or a BaseLLM")
+        messages: List[BaseMessage] = [SystemMessage(content=system_prompt)]
+        for prompt, expected_result in self.examples:
+            messages.append(HumanMessage(content=prompt))
+            messages.append(AIMessage(content=expected_result))
+        messages.append(HumanMessage(content=user_prompt))
+        output = _run_completion(llm, messages)
         return self.result_type.parse_raw(output).result
 
 
